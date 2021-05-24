d0f3f11146d88b0b51037c7e34a5af5fcdbdc62f
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-24 18:55:33 -0500

    Introdcción a variables y comentarios.

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,3 +1,29 @@
-console.log('Hola Mundo');
+//console.log('Hola Mundo');
 
-console.log(console.log('Hola Mundo 2'));
+//console.log(console.log('Hola Mundo 2'));
+
+/*Variable 
+
+-Es un contenedor de información que apunta a un lugar en memoria.
+-Dicha información puede cambiar en el futuro.
+
+*/
+//let a = 10;
+//var b = 10;
+/*Este "const", es una constante, una constante no puede cabiar su valor en un futuro.*/
+//const c = 10;
+// c = 20;  // no se puede hacer.
+/* Polyfill
+
+Es un código que provee el funcionamiento de una nueva característica de JavaScript (ES6), en versiones viejas 
+como ES5
+
+*/
+
+let a = 10,
+    b = 20,
+    c = 10,
+    d = 10,
+    x = a + b;
+
+console.log(x);
