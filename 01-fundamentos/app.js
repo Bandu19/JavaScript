8c759b78cf80a74a894ad525ba36ad6315e54ce8
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-24 20:05:02 -0500

    Introducción a la consola -clase 3 PARTE 1

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,29 +1,24 @@
 //console.log('Hola Mundo');
-
-//console.log(console.log('Hola Mundo 2'));
-
-/*Variable 
-
--Es un contenedor de información que apunta a un lugar en memoria.
--Dicha información puede cambiar en el futuro.
-
-*/
-//let a = 10;
-//var b = 10;
-/*Este "const", es una constante, una constante no puede cabiar su valor en un futuro.*/
-//const c = 10;
-// c = 20;  // no se puede hacer.
-/* Polyfill
-
-Es un código que provee el funcionamiento de una nueva característica de JavaScript (ES6), en versiones viejas 
-como ES5
-
-*/
-
 let a = 10,
     b = 20,
     c = 10,
     d = 10,
     x = a + b;
 
+
 console.log(x);
+// Consola de Advertencia
+console.warn(x);
+// Mensaje de Error 
+console.error(x);
+// Mensaje de Informe
+console.info(x);
+
+console.log('a', a);
+console.warn('b', b);
+console.info('La suma de a+b es igual =', x);
+
+// Otra forma de imprimir los Mensajes seria:
+console.log('%c Mis variables', 'color:red; font-weight: bold')
+console.log({ a });
+console.log({ d });
