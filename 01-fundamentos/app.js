015a8735839a654d1e3f9cc1cf83090bf58645a2
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-24 20:30:50 -0500

    Introducción a la consola -clase 3 PARTE 2

--- 01-fundamentos/app.js
+++ 01-fundamentos/app.js
@@ -1,24 +1,30 @@
 //console.log('Hola Mundo');
 let a = 10,
     b = 20,
-    c = 10,
-    d = 10,
+    c = 'Hola ',
+    d = 'Spiderman',
     x = a + b;
 
+//console.table({ a, b, c, d, x });
+/* HACE EN CONCATENAR LOS DOS VALORES C + D*/
+const saludo = c + d;
 
-console.log(x);
-// Consola de Advertencia
-console.warn(x);
-// Mensaje de Error 
-console.error(x);
-// Mensaje de Informe
-console.info(x);
+/*
+    TIIPOS DE MENSAJES 
+    console.log(x);
+    // Consola de Advertencia
+    console.warn(x);
+    // Mensaje de Error 
+    console.error(x);
+    // Mensaje de Informe
+    console.info(x);
 
-console.log('a', a);
-console.warn('b', b);
-console.info('La suma de a+b es igual =', x);
+    console.log('a', a);
+    console.warn('b', b);
+    console.info('La suma de a+b es igual =', x);
 
-// Otra forma de imprimir los Mensajes seria:
-console.log('%c Mis variables', 'color:red; font-weight: bold')
-console.log({ a });
-console.log({ d });
+    // Otra forma de imprimir los Mensajes seria:
+    console.log('%c Mis variables', 'color:red; font-weight: bold')
+    console.log({ a });
+    console.log({ d });
+*/
