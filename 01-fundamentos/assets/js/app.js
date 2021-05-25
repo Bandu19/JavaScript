9c6810d34771c30684f9c93cd0e1e4f8a338c030
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-24 22:26:48 -0500

    Principal Problema con la inicialización de variables con Var

--- 01-fundamentos/assets/js/app.js
+++ 01-fundamentos/assets/js/app.js
@@ -1,6 +1,11 @@
 //console.log('Hola Mundo');
 //alert('Hola desde app.js')
 
+/*ANTES TENEMOS QUE DECLARAR LAS VARIABLES EN ESTE CASO let Y DESPUES UTILIZAMOS console PARA LOS MENSAJES. */
+
+let miNovia = 'Mi novia Yasmin Soriano';
+console.log(miNovia + ' Gutierrez')
+
 let a = 10,
     b = 20,
     c = 'Hola ',
@@ -12,27 +17,3 @@ let a = 10,
 const saludo = c + d;
 
 c = 'Hola de nuevo';
-
-
-// var es una palabra reservada.
-let miNovia = 'Mi novia Yasmin Soriano';
-
-
-/* 
-        console.log(x);
-        // Consola de Advertencia
-        console.warn(x);
-        // Mensaje de Error 
-        console.error(x);
-        // Mensaje de Informe
-        console.info(x);
-
-        console.log('a', a);
-        console.warn('b', b);
-        console.info('La suma de a+b es igual =', x);
-
-        // Otra forma de imprimir los Mensajes seria:
-        console.log('%c Mis variables', 'color:red; font-weight: bold');
-        console.log({ a });
-        console.log({ d });
-*/
