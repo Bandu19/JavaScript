3ae725a090f54b5888218302be467bbc43a035e0
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-24 22:01:44 -0500

    Orden y lugar de las importaciones

---
+++ 01-fundamentos/assets/js/app.js
@@ -0,0 +1,38 @@
+//console.log('Hola Mundo');
+//alert('Hola desde app.js')
+
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Spiderman',
+    x = a + b;
+
+//console.table({ a, b, c, d, x });
+/* HACE EN CONCATENAR LOS DOS VALORES C + D*/
+const saludo = c + d;
+
+c = 'Hola de nuevo';
+
+
+// var es una palabra reservada.
+let miNovia = 'Mi novia Yasmin Soriano';
+
+
+/* 
+        console.log(x);
+        // Consola de Advertencia
+        console.warn(x);
+        // Mensaje de Error 
+        console.error(x);
+        // Mensaje de Informe
+        console.info(x);
+
+        console.log('a', a);
+        console.warn('b', b);
+        console.info('La suma de a+b es igual =', x);
+
+        // Otra forma de imprimir los Mensajes seria:
+        console.log('%c Mis variables', 'color:red; font-weight: bold');
+        console.log({ a });
+        console.log({ d });
+*/
