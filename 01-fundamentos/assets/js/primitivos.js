b71dcaa47958d30296aa2812db0b045a78984452
Oscar Gus <oscagustavo1961@gmail.com>
2021-05-25 23:59:00 -0500

    Introducción general a los tipos primitivos PARTE 2

---
+++ 01-fundamentos/assets/js/primitivos.js
@@ -0,0 +1,72 @@
+//STRING
+
+//FORMA CORRECTA
+let nombre = 'Steve ROGER';
+console.log(nombre);
+
+
+nombre = 'Yasmin Soriano';
+console.log(nombre);
+
+
+nombre = "Tia May";
+//Backtiks 
+nombre = `Tia May`;
+
+//TYPEOF significa describir que tipo de primitivo es la variable
+console.log(typeof nombre); //String
+
+nombre = 123;
+console.log(typeof nombre); //Number
+
+//BOOLEANOS
+
+let esMarvel = true; //false
+console.log(typeof esMarvel); //boolean
+
+//NUMEROS
+
+let edad = 33;
+console.log(typeof edad); // Number
+
+edad = 33.001;
+console.log(typeof edad); // Number
+
+
+let superPoder;
+console.log(typeof superPoder); // Undefined
+
+
+let soyNull = null;
+console.log(typeof soyNull); // Object 
+
+
+
+//SYMBOL
+
+let Symbol1 = Symbol('a');
+let Symbol2 = Symbol('a');
+
+console.log(typeof Symbol1); // symbol
+
+
+
+
+
+
+
+
+
+/*FORMA INCORRECTA 
+    let nombre2 = 'Rogelio';
+    let nombre2 = 'Pedro';
+
+    console.log(nombre2);
+OTRA FORMA INCORRECTA ES NO REINIZIALIZAR LAS VARIABLES CON VAR
+    var nombre2 = 'Rogelio';
+    var nombre2 = 'Pedro';
+
+    console.log(nombre2);
+
+
+*/
