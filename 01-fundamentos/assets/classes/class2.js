8124490963a725f952548937aa5f09cce0dade7a
Oscar Acuña <oscagustavo1961@gmail.com>
2021-10-11 23:02:48 -0500

    Clase 74. Sets y Gets

---
+++ 01-fundamentos/assets/classes/class2.js
@@ -0,0 +1,82 @@
+class Persona {
+  //ES UN METODO que creera Instancias
+  //Estos son Propiedades de clase
+  nombre = "";
+  codigo = "";
+  frase = "";
+  apellido = "";
+  //INSTANCIAS NO INCLUIDAS EN EL CONSTRUCTOR
+  comida = "";
+  juegos = "";
+
+  constructor(
+    nombre = "Sin Nombre",
+    codigo = "Sin Codigo",
+    frase = " Sin Frase",
+    apellido = " Sin Apellido"
+  ) {
+    //FORMA DE MANDAR MENSAJE DE ERROR EN LA CONSOLA
+    if (!nombre) throw Error("Nececitamos el nombre");
+    //d
+
+    this.nombre = nombre;
+    this.codigo = codigo;
+    this.frase = frase;
+    this.apellido = apellido;
+  }
+
+  /***************************************************************************/
+  /** EL METODO SET
+   *
+   */
+  /**El SET solo recibe un argumento*/
+  set setComidaFavorita(comida) {
+    //
+    this.comida = comida.toUpperCase();
+  }
+
+  set setJuegoFavorito(juego) {
+    this.juegos = juego.toUpperCase();
+  }
+
+  /** EL METODO GET
+   *
+   */
+
+  get getComidaFavorita() {
+    return `La comida Favorita de ${this.nombre} es ${this.comida} `;
+  }
+
+  /***************************************************************************/
+
+  /** Estos Son metodos */
+  queinSoy() {
+    console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}.`); //SE MANDA UN MENSAJE
+  }
+  miCodigo() {
+    this.queinSoy();
+    console.log(`${this.codigo} dice: ${this.frase}`);
+  }
+}
+
+/** LLAMAMOS AQUI LOS INSTANCIAS DETRO DE UNA CLASE QUE LLEVA DENTRO
+ * UN CONSTRUCTOSR
+ */
+
+const spiderman = new Persona(
+  "Peter Parker",
+  "Spiderman",
+  "Un gran poder conlleva a una responsabilidad"
+);
+
+spiderman.setComidaFavorita = "el pie de cereza de la tía May";
+
+/** ESTO NO SE DEBE DE HACER EN JAVASCRIPT
+ *
+ */
+// spiderman.nemesis = "Duende Verde";
+// spiderman.comida = "Duende Verde";
+
+// CONSOLA
+console.log(spiderman);
+console.log(spiderman.getComidaFavorita);
