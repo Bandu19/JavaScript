07ef17f36597a1b179703b3f6f85f6f87ceb2c02
Oscar Acuña <oscagustavo1961@gmail.com>
2021-10-11 22:20:59 -0500

    Clase 73 Métodos en las clases

--- 01-fundamentos/assets/classes/class.js
+++ 01-fundamentos/assets/classes/class.js
@@ -4,8 +4,9 @@ class Persona {
   nombre = "";
   codigo = "";
   frase = "";
+  apellido = "";
 
-  constructor(nombre, codigo, frase) {
+  constructor(nombre, codigo, frase, apellido) {
     // console.log("Hola!");
 
     //FORMA DE MANDAR MENSAJE DE ERROR EN LA CONSOLA
@@ -15,17 +16,55 @@ class Persona {
     this.nombre = nombre;
     this.codigo = codigo;
     this.frase = frase;
+    this.apellido = apellido;
+  }
+  /** Estos Son metodos */
+  queinSoy() {
+    console.log(`Soy ${this.nombre} y mi identidad es ${this.codigo}.`); //SE MANDA UN MENSAJE
+  }
+  miCodigo() {
+    this.queinSoy();
+    console.log(`${this.codigo} dice: ${this.frase}`);
+  }
+
+  //PRUEBA
+
+  quienEres() {
+    console.log(`Soy ${this.nombre} y mi codigo es ${this.frase}.`);
+  }
+
+  //OTHER EXERCISE
+
+  queinToca() {
+    console.log(
+      `Mi nombre es: ${this.nombre} y mi apellido es: ${this.apellido}`
+    );
   }
 }
+
+/** LLAMAMOS AQUI LOS INSTANCIAS DETRO DE UNA CLASE QUE LLEVA DENTRO
+ * UN CONSTRUCTOSR
+ *
+ */
 const batMan = new Persona("Oscar", 13, "TODOS ");
 const spiderman = new Persona(
   "Peter Parker",
-  "Spider",
+  "Spiderman",
   "Un gran poder conlleva a una responsabilidad"
 );
+const wife = new Persona("Yasmin Sori", "19", "Te vas a Morir Prro.");
+const husBand = new Persona("Oscar Gustavo", "-", "-", "Sostenes");
 
+/** MENSAJES EN CONSOLA */
 console.log(batMan);
 console.log(spiderman);
+console.log(husBand);
+/** AQUI EJECUTA TODOS LAS INSTANCIAS */
+spiderman.queinSoy();
+wife.miCodigo();
+batMan.queinSoy();
+wife.quienEres();
+husBand.queinToca();
 
 // const spiderman = new Persona();
 // console.log(spiderman);
