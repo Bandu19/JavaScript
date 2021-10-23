e3dff33fd762ac879ece232959142a1c5d7c043b
Oscar Acuña <oscagustavo1961@gmail.com>
2021-10-22 21:01:37 -0500

    Clase 85 Exposición del problema y nececidad del Webpack

---
+++ 03-webpack-inicial/src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Aqui damos una referencia
+ * e importamos la funcion dentro del otro archivo
+ * componentes.js
+ */
+
+import { saludar } from "./js/componentes";
+
+const nombre = "Fernando";
+saludar(nombre);
