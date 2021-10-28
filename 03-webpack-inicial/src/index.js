46b387f2332fa6c69601c1d3814a2244d4697416
Oscar Acuña <oscagustavo1961@gmail.com>
2021-10-28 14:28:10 -0500

    RETOMANDO CLASE 86

--- 03-webpack-inicial/src/index.js
+++ 03-webpack-inicial/src/index.js
@@ -1,10 +1,4 @@
-/**
- * Aqui damos una referencia
- * e importamos la funcion dentro del otro archivo
- * componentes.js
- */
-
 import { saludar } from "./js/componentes";
 
-const nombre = "Fernando";
+const nombre = "Oscar";
 saludar(nombre);
