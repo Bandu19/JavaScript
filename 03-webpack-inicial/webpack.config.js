1d495a9dc9833468e71e3dbbd30271c10813654f
Oscar Acuña <oscagustavo1961@gmail.com>
2021-10-28 19:32:04 -0500

    Clase 88 Webpack Deb Server

--- 03-webpack-inicial/webpack.config.js
+++ 03-webpack-inicial/webpack.config.js
@@ -2,12 +2,26 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
+  output: {
+    /**
+     * ESTE HACE QUE LIMPIE O ACTUALIZE TODO
+     * TU CARPETA DE TU DIST
+     */
+    clean: true,
+  },
+
   module: {
     rules: [
       {
+        /**correra cada vez que encuentre un HTML con el comando
+         * npm run build
+         */
+        //_    _/\.html$/i (EXPRESION REGULAR)  _//
         test: /\.html$/i,
+        /**Si lo encuentra LLAMAREMOS a la dependencia (html-loader)*/
         loader: "html-loader",
         options: {
+          /** */
           sources: false,
           minimize: false,
         },
@@ -15,8 +29,14 @@ module.exports = {
     ],
   },
   plugins: [
+    /** CONFIGURANDO UNA NUEVA INSTANCIA */
     new HtmlWebPackPlugin({
+      // title: "Mi WebPack App", PUEDES MODIFICAR EL TITULO DE TU INDEX.HTML de la carpeta dist
       template: "./src/index.html",
+      /**
+       * ESTO CONFIGURA EL NOMBRE DE TU ARCHIVO
+       * QUE ESTARA DENTRO DE LA CARPETA dist
+       */
       filename: "./index.html",
     }),
   ],
