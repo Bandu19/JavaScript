47ab39a95680da03c35675aa62a225b36ccc9e49
Oscar Acuña <oscagustavo1961@gmail.com>
2022-01-22 16:59:10 -0600

    Practica 2 parte 1(04-webpack-inicial)

--- 04-webpack-incial/src/index.js
+++ 04-webpack-incial/src/index.js
@@ -1,5 +1,25 @@
-import { saludar } from "./js/componentes";
-import "./styles.css";
-const nombre = "Oscar";
+// import { saludar } from "./js/componentes";
+// const nombre = "Oscar";
+// saludar(nombre);
 
-saludar(nombre);
+/**  INDEX QUE CONECTA CON LA CARPETA
+ *   (dist)
+ *
+ *
+ *  ===> index (RAIZ) <===
+ */
+
+import "./styles.css";
+//importación classes/index.js
+import { Todo, TodoList } from "./classes";
+
+const todoList = new TodoList(); //Muy importante
+
+const tarea = new Todo("HOLA MUNDO");
+const tarea2 = new Todo("SEGUNDO HOLA MUNDO");
+
+todoList.nuevoTodo(tarea); //SE CREA EL ARREGLO
+todoList.nuevoTodo(tarea2);
+
+//IMPRIMIR ARREGLO
+console.log("RESULTADO:", todoList);
