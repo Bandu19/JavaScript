d5369c571e1ddc38d75c5a3c387d27bc71f0e0cd
Oscar Acuña <oscagustavo1961@gmail.com>
2022-01-22 23:28:21 -0600

    Practica 2 parte 3

--- 04-webpack-incial/src/index.js
+++ 04-webpack-incial/src/index.js
@@ -11,23 +11,12 @@
 
 import "./styles.css";
 /**
- * importación classes/index.js
+ * importación ./classes/index.js
  */
 import { Todo, TodoList } from "./classes";
+/**
+ * importación ./js/componentes
+ */
 import { crearTodoHtml } from "./js/componentes";
 
-const todoList = new TodoList(); //Muy importante
-
-const tarea = new Todo("HOLA MUNDO");
-const tarea2 = new Todo("SEGUNDO HOLA MUNDO");
-const tarea3 = new Todo("Tercer HOLA MUNDO");
-
-todoList.nuevoTodo(tarea); //SE CREA EL ARREGLO
-todoList.nuevoTodo(tarea2);
-
-crearTodoHtml(tarea);
-crearTodoHtml(tarea2);
-crearTodoHtml(tarea3);
-
-//IMPRIMIR ARREGLO
-console.log("RESULTADO:", todoList);
+export const todoList = new TodoList(); //Muy importante
--- 04-webpack-incial/src/js/componentes.js
+++ 04-webpack-incial/src/js/componentes.js
@@ -1,5 +1,9 @@
+import { Todo } from "../classes";
+import { todoList } from "../index";
+
 // Referencia en el HTML
 const divTodoList = document.querySelector(".todo-list");
+const txtInput = document.querySelector(".new-todo");
 
 export const crearTodoHtml = (todo) => {
   const htmlTodo = `
@@ -21,3 +25,13 @@ export const crearTodoHtml = (todo) => {
 
   return div.firstElementChild;
 };
+
+txtInput.addEventListener("keyup", (event) => {
+  // console.log(event);
+  if (event.keyCode === 13 && txtInput.value.length > 0) {
+    const nuevoTodo = new Todo(txtInput.value);
+    todoList.nuevoTodo(nuevoTodo);
+    crearTodoHtml(nuevoTodo);
+    txtInput.value = "";
+  }
+});
