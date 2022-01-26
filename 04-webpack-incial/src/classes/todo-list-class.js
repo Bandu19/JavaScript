99533859e2c2ac6333e0e62f75a453667e13e1a7
Oscar Acuña <oscagustavo1961@gmail.com>
2022-01-26 16:14:42 -0600

    Practica 2 parte 7

--- 04-webpack-incial/src/classes/todo-list-class.js
+++ 04-webpack-incial/src/classes/todo-list-class.js
@@ -2,15 +2,20 @@
 
 export class TodoList {
   constructor() {
-    this.todos = [];
+    // this.todos = [];
+    this.cargarLocalStorage();
   }
 
   nuevoTodo(todo) {
     this.todos.push(todo);
+    // Despues de que insertamos al arreglo vacio
+    // se guardan los cambios
+    this.guardarLocalStorage();
   }
 
   eliminarTodo(id) {
     this.todos = this.todos.filter((todo) => todo.id != id);
+    this.guardarLocalStorage();
   }
 
   //A LA PAR
@@ -18,6 +23,7 @@ export class TodoList {
     for (const todo of this.todos) {
       if (todo.id == id) {
         todo.completado = !todo.completado;
+        this.guardarLocalStorage();
         break;
       }
     }
@@ -25,5 +31,32 @@ export class TodoList {
 
   eliminarCompletados() {
     this.todos = this.todos.filter((todo) => !todo.completado);
+    this.guardarLocalStorage();
+  }
+
+  //-----------------> CLASE (115) <----------------------------
+
+  guardarLocalStorage() {
+    // Guardar en el LocalStorage en JSON tipo String el arreglo
+
+    //Para conservar tu arreglo en la carpeta(LocalStorage)
+    localStorage.setItem("todo", JSON.stringify(this.todos));
+  }
+
+  cargarLocalStorage() {
+    // Para conservar los datos en el LocalStorage(arreglo)
+    // aqui se conserva en HTML los cambios.
+    /***
+     * Al principio no existira dentro del localStorage
+     * el valor todo es por eso que toda la información
+     * sera almacenada dentro del "else"
+     */
+    if (localStorage.getItem("todo")) {
+      this.todos = JSON.parse(localStorage.getItem("todo")); // STRING a Object
+      console.log("cargarLocal:", this.todos);
+      console.log(typeof this.todos);
+    } else {
+      this.todos = [];
+    }
   }
 }
--- 04-webpack-incial/src/index.js
+++ 04-webpack-incial/src/index.js
@@ -20,3 +20,5 @@ import { Todo, TodoList } from "./classes";
 import { crearTodoHtml } from "./js/componentes";
 
 export const todoList = new TodoList(); //Muy importante
+
+todoList.todos.forEach((todo) => crearTodoHtml(todo));
