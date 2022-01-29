39b00888e8ccd7ba434672e72ecbb9223919125d
Oscar Acuña <oscagustavo1961@gmail.com>
2022-01-29 14:11:38 -0600

    Cambios de la Clase 123 Callbacks

--- Promesas - callbacks/src/index.js
+++ Promesas - callbacks/src/index.js
@@ -3,7 +3,11 @@ import { bucarHeroe } from "./js/callbacks";
 
 const heroeId = "capi";
 
+//-----> CLASE-123 <-----
+
 // llamar una función
 bucarHeroe(heroeId, (heroe) => {
   console.log("1:", heroe);
 });
+
+console.log("Fin de programa");
